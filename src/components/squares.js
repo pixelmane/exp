d79ed117ae471aff97e7598b000d6be4f8ee16b4
JayHaji <jaymainframe@JayHajis-MacBook-Pro.local>
2022-06-16 16:34:23 -0400

    Initialize

---
+++ src/components/squares.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+let rowSize = 10;
+export class Squares extends  React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            colorArray: new Array(rowSize * rowSize).fill('blue'),
+            numberOfSquares: rowSize * rowSize,
+            myColor: 'black',
+            grid: '1px dashed grey',
+        }
+        this.handleClick = this.handleClick.bind(this)
+        this.handleTouchMove = this.handleTouchMove.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleColor = this.handleColor.bind(this)
+        this.handleSize = this.handleSize.bind(this)
+        this.handleToggle = this.handleToggle.bind(this)
+    }
+    handleClick(){
+        let colorArray = this.state.colorArray
+        colorArray[1] = 'green';
+        this.setState({
+            colorArray: colorArray
+        })
+    }
+    handleTouchMove(e) {
+        e.preventDefault()
+        let ouch = e.changedTouches[0];
+        let x;
+        let y;
+        x = ouch.clientX;
+        y = ouch.clientY;
+        let zzz = document.elementFromPoint(x,y);
+        
+        this.handleChange(zzz.value)
+    }
+    handleChange(changeMe){
+        let colorArray = this.state.colorArray
+        colorArray[changeMe] = this.state.myColor;
+        this.setState({
+            colorArray: colorArray,
+        })
+    }
+    handleColor(e){
+        let newColor = e.target.value;
+        this.setState({
+            myColor: newColor 
+        })
+    }
+    handleSize(e){
+        let reSize = e.target.value;
+        console.log(reSize)
+        rowSize = reSize
+        let total = reSize * reSize;
+        //let numberOfSquares = this.state.numberOfSquares 
+        //let colorArray = this.state.colorArray
+        this.setState({
+            numberOfSquares:  total,
+            colorArray: new Array(total).fill('white'),
+            grid: '1px dashed grey',
+        })
+       
+    }
+    handleToggle(){
+        if(this.state.grid === '1px dashed grey'){
+            this.setState({
+                grid: 'none',
+            })
+        } else {
+            this.setState({
+                grid: '1px dashed grey',
+            })
+        }
+       }
+    render() {
+        return (
+            <div>
+                <div id='container'>
+                <div onTouchMove={this.handleTouchMove} id="board">
+                <Square gridToggle={this.state.grid} colorArray={this.state.colorArray} />
+                </div>
+                <div id='controls'>
+                    <div id="title">
+                        <h1>RetroArtMuseum</h1>
+                    </div>
+                    <div id="colorContainer">
+                        <h2>Color:</h2>
+                        <input id="colorSelect" onChange={this.handleColor} type='color' value='black'></input>
+                    </div>  
+                    <div id='gridSize'>
+                        <h2>Grid Size:</h2>
+                        <button onClick={this.handleSize}  value={10}>10x10</button><button onClick={this.handleSize}  value={20}>20x20</button><button onClick={this.handleSize}  value={30}>30x30</button><button onClick={this.handleSize}  value={40}>40x40</button><button onClick={this.handleSize}  value={50}>50x50</button></div>
+                    <button id='gridToggle' onClick={this.handleToggle}>Grid Toggle</button>
+                    <h1>new font</h1>
+                </div>
+                </div>                
+            </div>
+        )
+    }
+    componentDidMount(){
+        document.addEventListener('DOMContentLoaded', convertSize())
+       window.addEventListener('resize', convertSize())
+    }
+    componentDidUpdate(){
+        document.addEventListener('DOMContentLoaded', convertSize())
+       window.addEventListener('resize', convertSize())
+    }
+}
+
+const Square = (props) => {
+   
+    let squareArray = []
+    for (let i = 0; i < props.colorArray.length; i++){
+        squareArray.push( <button value={i} className="square" style={{backgroundColor: props.colorArray[i], border: props.gridToggle}}></button>)
+    }
+    return (
+        [squareArray]
+    )
+}
+function convertSize () {
+    let size = window.innerHeight;
+    let size2 = window.innerWidth;
+    document.getElementById('board').style.height = `${size}px`
+    document.getElementById('board').style.width = `${size}px`
+    document.getElementById('controls').style.width = `${size2 - size}px`
+    
+    let sizeArray = document.getElementsByClassName('square')
+    for (let j = 0; j < sizeArray.length; j++){
+        sizeArray[j].style.width = `${size/rowSize}px`
+        sizeArray[j].style.height = `${size/rowSize}px`
+    }
+}
